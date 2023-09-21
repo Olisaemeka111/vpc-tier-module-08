ef6ec027d5874084762799a0abfade2918d61e00
Okawuma <kawumatk@gmail.com>
2023-09-21 13:53:08 +0100

    Addede ECS-role

--- appleSCH-website/main.tf
+++ appleSCH-website/main.tf
@@ -38,3 +38,8 @@ module "security_group" {
   source = "../modules/security-group"
   vpc_id = module.vpc.vpc_id
 }
+
+module "ECS-task-execution-role" {
+  source       = "../modules/ECS-task-execution-role"
+  project_name = module.vpc.project_name
+}
